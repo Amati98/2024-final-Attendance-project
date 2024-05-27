1ef9c0e21962fafcbb7833dfa88b1ddcd1f81fa7
Goodness Mbah <gmbah@nalosolutions.com>
2024-05-27 08:25:10 +0000

    profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,12 +1,38 @@
+import 'package:final_year/service/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends ConsumerWidget {
   const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('profile'),
+  Widget build(BuildContext context,WidgetRef ref) {
+    final user = ref.watch(userProvider);
+   if (user == null) {
+      return Scaffold(
+        appBar: AppBar(title: Text('User Profile')),
+        body: Center(child: Text('No user logged in')),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text('User Profile')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('First Name: ${user.fname}', style: TextStyle(fontSize: 18)),
+            Text('Surname: ${user.sname}', style: TextStyle(fontSize: 18)),
+            Text('Other Name: ${user.oname}', style: TextStyle(fontSize: 18)),
+            Text('Email: ${user.email}', style: TextStyle(fontSize: 18)),
+            Text('Phone: ${user.phone}', style: TextStyle(fontSize: 18)),
+            Text('Gender: ${user.gender}', style: TextStyle(fontSize: 18)),
+            Text('Address: ${user.address}', style: TextStyle(fontSize: 18)),
+            Text('Username: ${user.username}', style: TextStyle(fontSize: 18)),
+          ],
+        ),
+      ),
     );
   }
 }
